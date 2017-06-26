e66f2335fb51a40306f16fb5254cb51ceae12ffd
bad4iz <bad4iz@gmail.com>
2017-06-26 14:42:36 +0400

    r

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
         drawHMI(this.refs.theInput);
         console.log('im alive');
     }
-    
+
   render() {
         const myStyle = {
             position: 'absolute',
