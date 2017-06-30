b2f57adad373fe264dfdd8d80424f64a5514c4eb
bad4iz <bad4iz@gmail.com>
2017-06-30 11:57:37 +0400

    d

--- src/layout/menuElement.js
+++ src/layout/menuElement.js
@@ -1,13 +1,11 @@
 import React, {Component} from 'react';
-import {IconButton, Menu, MenuItem} from "react-mdl";
+import {IconButton, Menu, MenuItem, Textfield} from "react-mdl";
 import images from '../component/images.json';
-import { SelectField, Option } from 'react-mdl-selectfield';
+import {SelectField, Option} from 'react-mdl-selectfield';
 
 class menuElement extends Component {
 
-    state={
-
-    }
+    state = {}
 
     getElement = () => {
         const newElement = images.filter(item => {
@@ -15,6 +13,7 @@ class menuElement extends Component {
         });
         return newElement;
     };
+
     componentWillMount() {
         // this.props.imgUrl.substring(this.props.imgUrl.indexOf('img'));
         // this.setState
@@ -23,6 +22,7 @@ class menuElement extends Component {
         //     return item.url == this.props.imgUrl.substring(this.props.imgUrl.indexOf('img'));
         // })
     }
+
     handleSelected = (key) => {
         const newImage = images.filter(item => {
             return item.id == key;
@@ -31,11 +31,20 @@ class menuElement extends Component {
     }
 
     render() {
-      return (
+        return (
             <div style={{right: 0, position: 'absolute', zIndex: 9}}>
-                <IconButton name="more_vert" id={this.props.id + "demo-menu-lower-left"} />
-                <Menu target={this.props.id + "demo-menu-lower-left"} style={{ position: 'relative', height: '200px', width: '350px'}}>
-                    <SelectField onChange={ this.handleSelected } label={'Выбрать изображение'} value={2} >
+                <IconButton name="more_vert" id={this.props.id + "demo-menu-lower-left"}/>
+                <Menu target={this.props.id + "demo-menu-lower-left"}
+                      style={{position: 'relative', height: '200px', width: '350px'}}>
+                    <div>
+                        <Textfield
+                            onChange={() => {}}
+                            label="Text..."
+                            style={{width: '200px'}}
+                        />
+
+                    </div>
+                    <SelectField onChange={ this.handleSelected } label={'Выбрать изображение'} value={2}>
 
                         {images.map(user =>
                             <Option key={user.id} value={user.id}>
