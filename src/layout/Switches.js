57e8f57365be0f21a44b4fbfa1931241a8952073
bad4iz <bad4iz@gmail.com>
2017-06-30 22:52:02 +0400

    LabelSwitch

--- src/layout/Switches.js
+++ src/layout/Switches.js
@@ -2,6 +2,7 @@
 
 import React, { Component } from 'react';
 import Switch from 'material-ui/Switch';
+import { LabelSwitch } from 'material-ui/Switch';
 
 export default class Switches extends Component {
 
@@ -12,7 +13,7 @@ export default class Switches extends Component {
     render() {
         return (
             <div>
-                <Switch
+                <LabelSwitch
                     checked={this.props.cannotChange}
                     onChange={this.onChangeHandler}
                     aria-label="checkedA"
