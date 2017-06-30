62b53d4eb8a44c6d807d578e43bb1f1fe9674fe5
bad4iz <bad4iz@gmail.com>
2017-06-30 16:46:48 +0400

    switch

--- src/layout/Switches.js
+++ src/layout/Switches.js
@@ -1,3 +1,30 @@
-/**
- * Created by bad4i on 30.06.2017.
- */
+// @flow
+
+import React, { Component } from 'react';
+import Switch from 'material-ui/Switch';
+
+export default class Switches extends Component {
+    state = {
+        checkedA: true,
+    };
+
+    render() {
+        return (
+            <div>
+                <Switch
+                    checked={this.state.checkedA}
+                    onChange={(event, checked) => this.setState({ checkedA: checked })}
+                    aria-label="checkedA"
+                    label="A"
+                />
+                {/*<Switch*/}
+                    {/*checked={this.state.checkedB}*/}
+                    {/*onChange={(event, checked) => this.setState({ checkedB: checked })}*/}
+                    {/*aria-label="checkedB"*/}
+                {/*/>*/}
+                {/*<Switch checked={this.state.checkedC} aria-label="checkedC" disabled />*/}
+                {/*<Switch checked={this.state.checkedD} aria-label="checkedD" disabled />*/}
+            </div>
+        );
+    }
+}
